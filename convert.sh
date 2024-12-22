6e910abe0a1ad9c6003ce5153bf92d63509cc6df
seleb <seleb@users.noreply.github.com>
2024-12-22 19:45:11 +0000

    deploy: 2d6200592db218ef4f75df4900d01312348b525b

--- convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in ./assets/video/*.gif
-	do ffmpeg -y -i "$i" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "${i%.*}.mp4";
-done
-
-for i in ./assets/video/*.mp4
-	do ffmpeg -i "$i" -frames:v 1 "${i%.*}.png";
-done
