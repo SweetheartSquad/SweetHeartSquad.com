ca405f4966b92d316f5a42b4e8b7c7fb182aea51
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2025-01-27 20:42:51 -0500

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in ./assets/video/*.gif
+for i in ./public/assets/video/*.gif
 	do ffmpeg -y -i "$i" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "${i%.*}.mp4";
 done
 
-for i in ./assets/video/*.mp4
+for i in ./public/assets/video/*.mp4
 	do ffmpeg -i "$i" -frames:v 1 "${i%.*}.png";
 done
