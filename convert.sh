beace19f51bef5a8d264425d1f73b384e145a1ea
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2025-02-02 11:21:39 -0500

    add ffmpeg check

--- convert.sh
+++ convert.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! [ -x "$(command -v ffmpeg)" ]; then
+  echo 'Error: ffmpeg was not found, go get it https://www.ffmpeg.org/download.html' >&2
+  exit 1
+fi
+
 for i in ./public/assets/video/*.gif
 	do ffmpeg -y -i "$i" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "${i%.*}.mp4";
 done
