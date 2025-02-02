c8eeee8a528c983a736f30002f35275010878471
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2025-02-02 12:44:08 -0500

    only keep mp4 if it's smaller than original gif

--- convert.sh
+++ convert.sh
@@ -5,11 +5,20 @@ if ! [ -x "$(command -v ffmpeg)" ]; then
   exit 1
 fi
 
-for i in ./public/assets/video/*.gif
-	do ffmpeg -y -i "$i" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "${i%.*}.mp4";
-	rm "$i";
+for i in ./public/assets/video/*.gif; do
+	ffmpeg -y -i "$i" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "${i%.*}.mp4";
+	sizeGif=`stat -c%s "$i"`
+	sizeVid=`stat -c%s "${i%.*}.mp4"`
+	if (($sizeGif>$sizeVid));then
+		rm "$i"
+	else
+		rm "${i%.*}.mp4"
+	fi
 done
 
-for i in ./public/assets/video/*.mp4
-	do ffmpeg -i "$i" -frames:v 1 "${i%.*}.png";
+for i in ./public/assets/video/*.gif; do
+	ffmpeg -y -i "$i" -frames:v 1 "${i%.*}.png";
+done
+for i in ./public/assets/video/*.mp4; do
+	ffmpeg -y -i "$i" -frames:v 1 "${i%.*}.png";
 done
